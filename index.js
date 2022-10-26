2120849f0608cca23d7eb868d620bac0e0a04d03
lanad01 <62043299+lanad01@users.noreply.github.com>
2022-10-26 21:32:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from './src/test_sangwoo/testNavi';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
